93bfdacb89ce0ac36bc56c1d9e2507a92992b617
mtdwijaya <mtiasdw44@gmail.com>
2026-07-02 17:16:04 +0700

    fix: correct typo in footer copyright text update : sidebar add postman

--- components/layouts/footer.tsx
+++ components/layouts/footer.tsx
@@ -33,7 +33,7 @@ export default function Footer() {
           "
         >
           © {new Date().getFullYear()} LRT
-          Jabodebek - mtdwidjaya
+          Jabodebek - mtdwijaya
         </span>
       </div>
     </footer>
